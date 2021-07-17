994ed4926fc9a90de21fbc684629414cee081744
Dans-sys <danielsolano712@gmail.com>
2021-07-16 22:34:46 -0600

    Prueba latitud y longitud

--- app/src/main/java/com/aplicacion/pm2e1grupo3/Pantalla1.java
+++ app/src/main/java/com/aplicacion/pm2e1grupo3/Pantalla1.java
@@ -7,31 +7,42 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.provider.Settings;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class Pantalla1 extends AppCompatActivity {
 
     Button btnsalvar, btnsalvados, btnmapa, btnfoto;
     EditText etnombre, ettelefono, etlatitud, etlongitud;
     ImageView ObjImagen;
-
+    TextView txtlat, txtlong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +56,8 @@ public class Pantalla1 extends AppCompatActivity {
         ettelefono = (EditText)findViewById(R.id.ettelefono);
         etlatitud = (EditText)findViewById(R.id.etlatitud);
         etlongitud = (EditText)findViewById(R.id.etlongitud);
+        txtlat = (TextView) findViewById(R.id.txtlatitud);
+        txtlong = (TextView) findViewById(R.id.txtlongitud);
 
         ObjImagen = (ImageView) findViewById(R.id.fotografia);
         btnfoto = (Button) findViewById(R.id.btnfoto);
@@ -65,12 +78,6 @@ public class Pantalla1 extends AppCompatActivity {
             }
         });
 
-        String lgt = getIntent().getStringExtra("longitud");
-        String ltt = getIntent().getStringExtra("latitud");
-
-        etlongitud.setText(""+lgt);
-        etlatitud.setText(""+ltt);
-
         btnsalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,6 +94,110 @@ public class Pantalla1 extends AppCompatActivity {
             }
         });
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
+    }
+
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setPantalla1(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+
+        txtlat.setText("Localización agregada");
+        txtlong.setText("");
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                    txtlong.setText("Mi direccion es: \n"
+                            + DirCalle.getAddressLine(0));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class Localizacion implements LocationListener {
+        Pantalla1 pantalla1;
+
+        public Pantalla1 getPantalla1() {
+            return pantalla1;
+        }
+
+        public void setPantalla1(Pantalla1 pantalla1) {
+            this.pantalla1 = pantalla1;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            loc.getLatitude();
+            loc.getLongitude();
+
+            String Text1 =  ""+loc.getLatitude();
+            String Text2 =  ""+loc.getLongitude();
+            txtlat.setText(Text1);
+            txtlong.setText(Text2);
+            this.pantalla1.setLocation(loc);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            txtlat.setText("GPS Desactivado");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            txtlat.setText("GPS Activado");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
     }
 
     private void  abrirCamara(){
