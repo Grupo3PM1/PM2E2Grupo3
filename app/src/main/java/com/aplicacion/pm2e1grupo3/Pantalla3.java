694fc27cdde5821de71407c4937ad3f1310a3ee9
Dans-sys <danielsolano712@gmail.com>
2021-07-16 21:08:10 -0600

    tercer pantalla, mapa

--- app/src/main/java/com/aplicacion/pm2e1grupo3/Pantalla3.java
+++ app/src/main/java/com/aplicacion/pm2e1grupo3/Pantalla3.java
@@ -1,5 +1,6 @@
 package com.aplicacion.pm2e1grupo3;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
@@ -23,15 +24,6 @@ public class Pantalla3 extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private ActivityPantalla3Binding binding;
 
-    private Marker marcador;
-    private static final float camera_zoom = 15;
-    ImageView imgmarker;
-    private BottomSheetBehavior mBottomSheetBehavior1;
-    LinearLayout tapactionlayout;
-    View white_forground_view;
-    View bottomSheet;
-    TextView txtnombre_local, txtDireccion, txtHorario;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,43 +35,6 @@ public class Pantalla3 extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        View headerLayout1 = findViewById(R.id.bottomJsoft);
-        imgmarker = (ImageView)findViewById(R.id.ImgMarker);
-        txtnombre_local = (TextView)findViewById(R.id.txtNombreLocal);
-        txtDireccion = (TextView)findViewById(R.id.txtDireccion);
-        txtHorario = (TextView)findViewById(R.id.txtHorario);
-        tapactionlayout =(LinearLayout)findViewById(R.id.tap_action_layout);
-        bottomSheet = findViewById(R.id.bottomJsoft);
-        mBottomSheetBehavior1 = BottomSheetBehavior.from(bottomSheet);
-        mBottomSheetBehavior1.setPeekHeight(120);
-        mBottomSheetBehavior1.setState(BottomSheetBehavior.STATE_COLLAPSED);
-        mBottomSheetBehavior1.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
-                    tapactionlayout.setVisibility(View.VISIBLE);
-                }
-                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
-                    tapactionlayout.setVisibility(View.GONE);
-                }
-                if (newState == BottomSheetBehavior.STATE_DRAGGING) {
-                    tapactionlayout.setVisibility(View.GONE);
-                }
-            }
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-            }
-        });
-        tapactionlayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mBottomSheetBehavior1.getState()==BottomSheetBehavior.STATE_COLLAPSED)
-                {
-                    mBottomSheetBehavior1.setState(BottomSheetBehavior.STATE_EXPANDED);
-                }
-            }
-        });
     }
 
     /**
